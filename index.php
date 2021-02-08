fbe0518d488f134486c483d3dcd9f297716b7688
190103477 <78725610+190103477@users.noreply.github.com>
2021-02-09 01:09:48 +0600

    Update and rename hello.txt to index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>Hello World!</h2>";
+echo "My name is Inabat<br>";
+echo "I'm a 2nd year student<br>";
+echo "I want to become a computer programmer";
+?> 
+
+</body>
+</html>
